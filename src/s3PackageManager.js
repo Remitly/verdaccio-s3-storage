d3b311cdeee93b9b9a66e3a61f79eb35809c377e
liuyuewen91 <liuyuewen91@gmail.com>
2019-02-25 22:38:18 +0100

    support private s3 Bucket

--- src/s3PackageManager.js
+++ src/s3PackageManager.js
@@ -22,11 +22,21 @@ export default class S3PackageManager implements ILocalPackageManager {
     this.config = config;
     this.packageName = packageName;
     this.logger = logger;
-    this.s3 = new S3({
-      endpoint: this.config.endpoint,
-      region: this.config.region,
-      s3ForcePathStyle: this.config.s3ForcePathStyle
-    });
+    if(this.config.accessKeyId && this.config.secretAccessKey){
+      this.s3 = new S3({
+        endpoint: this.config.endpoint,
+        region: this.config.region,
+        s3ForcePathStyle: this.config.s3ForcePathStyle,
+        accessKeyId: this.config.accessKeyId,
+        secretAccessKey: this.config.secretAccessKey
+      });
+    }else{
+      this.s3 = new S3({
+        endpoint: this.config.endpoint,
+        region: this.config.region,
+        s3ForcePathStyle: this.config.s3ForcePathStyle
+      });
+    }
   }
 
   updatePackage(name: string, updateHandler: Callback, onWrite: Callback, transformPackage: Function, onEnd: Callback) {
