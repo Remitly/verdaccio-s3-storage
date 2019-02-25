46f3cb09d46b5464dbe634b1b63abc34cc400e96
Yuewen Liu <yliu@hms-dev.com>
2019-02-25 22:04:54 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,11 +25,21 @@ export default class S3Database implements ILocalData {
     }
     const configKeyPrefix = this.config.keyPrefix;
     this.config.keyPrefix = configKeyPrefix != null ? (configKeyPrefix.endsWith('/') ? configKeyPrefix : `${configKeyPrefix}/`) : '';
-    this.s3 = new S3({
-      endpoint: this.config.endpoint,
-      region: this.config.region,
-      s3ForcePathStyle: this.config.s3ForcePathStyle
-    });
+    if(this.config.accessKeyId&&this.secretAccessKey){
+      this.s3 = new S3({
+        endpoint: this.config.endpoint,
+        region: this.config.region,
+        s3ForcePathStyle: this.config.s3ForcePathStyle,
+        accessKeyId: this.config.accessKeyId,
+        secretAccessKey: this.config.secretAccessKey
+      });
+    }else{
+      this.s3 = new S3({
+        endpoint: this.config.endpoint,
+        region: this.config.region,
+        s3ForcePathStyle: this.config.s3ForcePathStyle
+      });
+    }
   }
 
   async getSecret(): Promise<any> {
